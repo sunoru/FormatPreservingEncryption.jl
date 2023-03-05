d9834f0f42c4889dc93d4485312b25608b75e7f5
スノル <s@sunoru.com>
2023-03-05 17:43:40 +0000

    Polish code.

--- src/ff1.jl
+++ src/ff1.jl
@@ -16,91 +16,83 @@ end
 FF1(; key_size=128, key, radix, tweak="") =
     FF1(key_size, key, radix, tweak)
 
-macro _ff1_impl(ctx, input, is_encrypt)
+function ff1_impl(ctx, input, is_encrypt)
     # TODO: optimize it
-    quote
-        ctx = $(esc(ctx))
-        input = $(esc(input))
-        aes_key = ctx.aes_key
-        tweak = ctx.tweak
-        radix = ctx.radix
-        n = length(input) |> UInt32
-        t = length(tweak) |> UInt32
-        # 1
-        u = n ÷ UInt32(2)
-        v = n - u
-        powru = BigInt(radix)^u
-        powrv = BigInt(radix)^v
-        # 2
-        out = UInt32.(input)
-        A = @view out[1:u]
-        B = @view out[u+1:end]
-        # 3
-        b = cld(ceil(UInt32, v * log2(radix)), 8)
-        # 4
-        d = UInt32(4) * cld(b, UInt32(4)) + UInt32(4)
+    aes_key = ctx.aes_key
+    tweak = ctx.tweak
+    radix = ctx.radix
+    n = length(input) |> UInt32
+    t = length(tweak) |> UInt32
+    # 1
+    u = n ÷ UInt32(2)
+    v = n - u
+    powru = BigInt(radix)^u
+    powrv = BigInt(radix)^v
+    # 2
+    out = UInt32.(input)
+    A = @view out[1:u]
+    B = @view out[u+1:end]
+    # 3
+    b = cld(ceil(UInt32, v * log2(radix)), 8)
+    # 4
+    d = UInt32(4) * cld(b, UInt32(4)) + UInt32(4)
 
-        # 5
-        P = (
-            0x1, 0x2, 0x1,
-            be_bytes((radix << UInt32(8)) | UInt32(10))...,
-            u % UInt8,
-            be_bytes(n)...,
-            be_bytes(t)...
-        )
+    # 5
+    P = (
+        0x1, 0x2, 0x1,
+        be_bytes((radix << UInt32(8)) | UInt32(10))...,
+        u % UInt8,
+        be_bytes(n)...,
+        be_bytes(t)...
+    )
 
-        pad = (-t - b - UInt32(1)) % UInt32(16)
-        Q = zeros(UInt8, t + pad + UInt32(1) + b)
-        S = zeros(UInt8, cld(d, 16) * 16)
-        # 6
-        for iter in UInt8(0):UInt8(9)
-            i = $(is_encrypt ? :iter : :(UInt8(9) - iter))
-            # 6.i
-            Q[1:t] .= tweak
-            Q[t+1:t+pad] .= 0x00
-            Q[t+pad+1] = i
-            B_num = num_in_base($(is_encrypt ? :B : :A), radix)
-            B_bytes = be_bytes(B_num, b)
-            B_bytes_len = length(B_bytes)
-            Q[t+pad+2:end-B_bytes_len] .= 0x00
-            Q[end-B_bytes_len+1:end] .= B_bytes
-            # 6.ii
-            R = encrypt(aes_key, to_uint128(P))
-            R = prf(aes_key, Q, R)
-            # 6.iii
-            S[1:16] .= le_bytes(R)
-            for j in 1:cld(d, 16)-1
-                j = UInt128(j)
-                tmp = be_bytes(j)
-                Sj = encrypt(aes_key, R ⊻ to_uint128(tmp))
-                S[16j+1:16j+16] .= le_bytes(Sj)
-            end
-            # 6.iv
-            y = num_in_base(S[1:d], 256)
-            # 6.v
-            m, powrm = if i & 1 == 0
-                u, powru
-            else
-                v, powrv
-            end
-            # 6.vi
-            c = mod(
-                num_in_base($(is_encrypt ? :A : :B), radix) +
-                $(is_encrypt ? :y : :(-y)),
-                powrm
-            )
-            # 6.vii, 6.viii & 6.ix
-            A, B = B, A
-            str_in_base!($(is_encrypt ? :B : :A), c, radix)
+    pad = (-t - b - UInt32(1)) % UInt32(16)
+    Q = zeros(UInt8, t + pad + UInt32(1) + b)
+    S = zeros(UInt8, cld(d, 16) * 16)
+    # 6
+    for iter in UInt8(0):UInt8(9)
+        i = is_encrypt ? iter : (UInt8(9) - iter)
+        # 6.i
+        Q[1:t] .= tweak
+        Q[t+1:t+pad] .= 0x00
+        Q[t+pad+1] = i
+        B_num = num_in_base(is_encrypt ? B : A, radix)
+        B_bytes = be_bytes(B_num, b)
+        B_bytes_len = length(B_bytes)
+        Q[t+pad+2:end-B_bytes_len] .= 0x00
+        Q[end-B_bytes_len+1:end] .= B_bytes
+        # 6.ii
+        R = encrypt(aes_key, to_uint128(P))
+        R = prf(aes_key, Q, R)
+        # 6.iii
+        S[1:16] .= le_bytes(R)
+        for j in 1:cld(d, 16)-1
+            j = UInt128(j)
+            tmp = be_bytes(j)
+            Sj = encrypt(aes_key, R ⊻ to_uint128(tmp))
+            S[16j+1:16j+16] .= le_bytes(Sj)
         end
-        out
+        # 6.iv
+        y = num_in_base(S[1:d], 256)
+        # 6.v
+        m, powrm = if i & 1 == 0
+            u, powru
+        else
+            v, powrv
+        end
+        # 6.vi
+        c = mod(
+            num_in_base(is_encrypt ? A : B, radix) +
+            (is_encrypt ? y : -y),
+            powrm
+        )
+        # 6.vii, 6.viii & 6.ix
+        A, B = B, A
+        str_in_base!(is_encrypt ? B : A, c, radix)
     end
+    out
 end
 
-function AESNI.encrypt(ctx::FF1, plain::AbstractArray{<:Integer})
-    @_ff1_impl(ctx, plain, true)
-end
+AESNI.encrypt(ctx::FF1, plain::AbstractArray{<:Integer}) = ff1_impl(ctx, plain, true)
 
-function AESNI.decrypt(ctx::FF1, cipher::AbstractArray{<:Integer})
-    @_ff1_impl(ctx, cipher, false)
-end
+AESNI.decrypt(ctx::FF1, cipher::AbstractArray{<:Integer}) = ff1_impl(ctx, cipher, false)
--- src/ff3.jl
+++ src/ff3.jl
@@ -17,66 +17,58 @@ end
 FF3(; key_size=128, key, radix, tweak="") =
     FF3(key_size, key, radix, tweak)
 
-macro _ff3_impl(ctx, input, is_encrypt)
-    quote
-        ctx = $(esc(ctx))
-        input = $(esc(input))
-        aes_key = ctx.aes_key
-        tweak = ctx.tweak
-        radix = ctx.radix
-        n = length(input) |> UInt32
-        t = length(tweak) |> UInt32
-        # 1
-        u = cld(n, UInt32(2))
-        v = n - u
-        powru = BigInt(radix)^u
-        powrv = BigInt(radix)^v
-        # 2
-        out = UInt32.(input)
-        A = @view out[1:u]
-        B = @view out[u+1:end]
-        # 3
-        T_L, T_R = tweak[1:4], tweak[5:8]
-        # 4
-        for iter in UInt8(0):UInt8(7)
-            i = $(is_encrypt ? :iter : :(UInt8(7) - iter))
-            # 4.i
-            m, powrm, W = if i & 1 == 0
-                u, powru, T_R
-            else
-                v, powrv, T_L
-            end
-            # 4.ii
-            B_num = num_in_base($(is_encrypt ? :B : :A), radix, bigendian=false)
-            B_bytes = le_bytes(B_num, m)
-            P_REVB = (
-                B_bytes...,
-                Iterators.repeated(UInt8(0), 12 - length(B_bytes))...,
-                W[4] ⊻ i,
-                W[3:-1:1]...,
-            )
-            # 4.iii
-            S = encrypt(aes_key, P_REVB)
-            # 4.iv
-            y = num_in_base(S, 256, bigendian=false)
-            # 4.v
-            c = mod(
-                num_in_base($(is_encrypt ? :A : :B), radix, bigendian=false) +
-                $(is_encrypt ? :y : :(-y)),
-                powrm
-            )
-            # 4.vi, 4.vii & 4.viii
-            A, B = B, A
-            str_in_base!($(is_encrypt ? :B : :A), c, radix, bigendian=false)
+function ff3_impl(ctx, input, is_encrypt)
+    aes_key = ctx.aes_key
+    tweak = ctx.tweak
+    radix = ctx.radix
+    n = length(input) |> UInt32
+    t = length(tweak) |> UInt32
+    # 1
+    u = cld(n, UInt32(2))
+    v = n - u
+    powru = BigInt(radix)^u
+    powrv = BigInt(radix)^v
+    # 2
+    out = UInt32.(input)
+    A = @view out[1:u]
+    B = @view out[u+1:end]
+    # 3
+    T_L, T_R = tweak[1:4], tweak[5:8]
+    # 4
+    for iter in UInt8(0):UInt8(7)
+        i = is_encrypt ? iter : (UInt8(7) - iter)
+        # 4.i
+        m, powrm, W = if i & 1 == 0
+            u, powru, T_R
+        else
+            v, powrv, T_L
         end
-        out
+        # 4.ii
+        B_num = num_in_base(is_encrypt ? B : A, radix, bigendian=false)
+        B_bytes = le_bytes(B_num, m)
+        P_REVB = (
+            B_bytes...,
+            Iterators.repeated(UInt8(0), 12 - length(B_bytes))...,
+            W[4] ⊻ i,
+            W[3:-1:1]...,
+        )
+        # 4.iii
+        S = encrypt(aes_key, P_REVB)
+        # 4.iv
+        y = num_in_base(S, 256, bigendian=false)
+        # 4.v
+        c = mod(
+            num_in_base(is_encrypt ? A : B, radix, bigendian=false) +
+            (is_encrypt ? y : -y),
+            powrm
+        )
+        # 4.vi, 4.vii & 4.viii
+        A, B = B, A
+        str_in_base!(is_encrypt ? B : A, c, radix, bigendian=false)
     end
+    out
 end
 
-function AESNI.encrypt(ctx::FF3, plain::AbstractArray{<:Integer})
-    @_ff3_impl(ctx, plain, true)
-end
+AESNI.encrypt(ctx::FF3, plain::AbstractArray{<:Integer}) = ff3_impl(ctx, plain, true)
 
-function AESNI.decrypt(ctx::FF3, cipher::AbstractArray{<:Integer})
-    @_ff3_impl(ctx, cipher, false)
-end
+AESNI.decrypt(ctx::FF3, cipher::AbstractArray{<:Integer}) = ff3_impl(ctx, cipher, false)
